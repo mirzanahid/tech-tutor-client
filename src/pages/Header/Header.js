b617a7d00ccf48c08dba40b71c908e54e721d1c7
mirzanahid <mirzasaikat3@gmail.com>
2022-10-25 22:00:15 +0600

    navbar added

--- src/pages/Header/Header.js
+++ src/pages/Header/Header.js
@@ -1,9 +1,30 @@
 import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import { Link } from "react-router-dom";
+
 
 
 const Header = () => {
     return (
-     <h1>this is header</h1>
+        <Navbar bg="light" expand="lg">
+        <Container>
+          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+           
+              <Nav.Link><Link>Home</Link></Nav.Link>
+              <Nav.Link><Link>Our Courses</Link></Nav.Link>
+              <Nav.Link><Link>Faq</Link></Nav.Link>
+              <Nav.Link><Link>Blog</Link></Nav.Link>
+              <Nav.Link><Link>Contact</Link></Nav.Link>
+              <Link>Log in</Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     );
 };
 
